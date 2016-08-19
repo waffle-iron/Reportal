4fcaadbe77d2b7b98fd3f6abd8774c52e75ac9e0
Cristian Muñoz <cmunozd@laaraucana.cl>
2016-08-19 16:11:17 -0300

    Maquena Colocaciones

---
+++ Reportal.Web/Controllers/ColocacionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Principal;
+using Reportal.Web.Models.Caller;
+namespace Reportal.Web.Controllers
+{
+    public class ColocacionController : Controller
+    {
+        private ColocCaller col;
+        // GET: Colocacion
+
+        public ColocacionController()
+        {
+            col = new ColocCaller();
+        }
+        public ActionResult Colocaciones()
+        {
+            return View();
+        }
+    }
+}
