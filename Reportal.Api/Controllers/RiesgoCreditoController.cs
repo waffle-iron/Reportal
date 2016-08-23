acd6c53f5aa5b56c984ec21c47f37479fd771d18
Carlos Pradenas <carlpradenas@gmail.com>
2016-08-23 15:54:28 -0300

    Indicador de Riesgo de Credito

---
+++ Reportal.Api/Controllers/RiesgoCreditoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Reportal.Api.Models;
+using Reportal.Domain;
+using System.Web.Http.Cors;
+
+
+namespace Reportal.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+    public class RiesgoCreditoController : ApiController
+    {
+        private RiesgoCreditoRepository RCRepo;
+
+        public RiesgoCreditoController()
+        {
+            RCRepo = new RiesgoCreditoRepository();
+        }
+
+        // GET: api/IndRiesgoCredito/201606
+        public IEnumerable<IndRiesgoCredito> Get(int id)
+        {
+            return RCRepo.Obtener(id);
+        }
+
+    }
+}
---
+++ Reportal.Api/Models/RiesgoCreditoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Reportal.Data;
+using Reportal.Domain;
+
+namespace Reportal.Api.Models
+{
+    public class RiesgoCreditoRepository
+    {
+        public List<IndRiesgoCredito> Obtener(int Periodo)
+        {
+            return IndRiesgoCreditoDataAccess.Obtener(Periodo);
+        }
+
+        public List<int> ObtenerPeriodos()
+        {
+            return IndRiesgoCreditoDataAccess.ObtenerPeriodos();
+        }
+    }
+}
