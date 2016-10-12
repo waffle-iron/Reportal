a83221532c882902984f5857c27ea5af9b3c4427
Carlos Pradenas <carlpradenas@gmail.com>
2016-10-12 18:47:46 -0300

    Seguimiento de preaprobados -> se trabaja en las tablas y obtencion de datos de estas

---
+++ Reportal.Api/Controllers/SegPreaprobadosPorCanalesPropensionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Reportal.Domain;
+using Reportal.Data;
+using System.Web.Http.Cors;
+
+namespace Reportal.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+    public class SegPreaprobadosPorCanalesPropensionController : ApiController
+    {
+        // GET: api/SegPreaprobadosPorCanalesPropension/
+        public IEnumerable<SegPreaprobadosFull> Get()
+        {
+            return SegPreaprobadosGlobalDataAccess.ListarByCanalesPropension(201609);
+        }
+        
+    }
+}
---
+++ Reportal.Api/Controllers/SegPreaprobadosPorNominasPropController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Reportal.Domain;
+using Reportal.Data;
+using System.Web.Http.Cors;
+
+namespace Reportal.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+    public class SegPreaprobadosPorNominasPropController : ApiController
+    {
+        // GET: api/SegPreaprobadosPorNominasProp/
+        public IEnumerable<SegPreaprobadosFull> Get()
+        {
+            return SegPreaprobadosGlobalDataAccess.ListarByNominayPropension(201609);
+        }
+    }
+}
