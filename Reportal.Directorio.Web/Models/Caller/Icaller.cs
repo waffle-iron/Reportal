82dcb172d12aa52d9d5f9cf7b08446a4aee84026
Cristian Muñoz <cmunozd@laaraucana.cl>
2016-10-14 11:33:09 -0300

    a

---
+++ Reportal.Directorio.Web/Models/Caller/Icaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reportal.Directorio.Web.Models.Caller
+{
+    public interface Icaller
+    {
+        List<T> Get<T>();
+        T Get<T>(int id);
+        void Create<T>(T o);
+        void Update<T>(int id, T o);
+        void Delete(int id);
+    }
+}
