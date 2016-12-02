c2e0e53cfe570fac6ceec1e33f6ab3666d724504
Carlos Pradenas <carlpradenas@gmail.com>
2016-12-02 11:35:43 -0300

    Avances de pantalla de nominas

---
+++ Reportal.Directorio.Web/Controllers/CampaniasController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reportal.Directorio.Web.Controllers
+{
+    public class CampaniasController : Controller
+    {
+        // GET: Campanias
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Nominas()
+        {
+            return View();
+        }
+
+    }
+}
